2c96491cf6ea4c1884717ea4837d99b3c3d00241
Tejas Patade <tej.patade69@gmail.com>
2023-09-11 20:07:15 +0530

    Setting Up...

---
+++ Sample.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int var;
+  cout << "Enter Your Name: ";
+  cin >> var;
+
+  // 0 denotes no error to OS, any other value means the program encountered an error
+  return 0;
+}
