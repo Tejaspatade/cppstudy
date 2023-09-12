f7af7cc1923e9f79ecb1553c0e8b1d04f2e51d01
Tejas Patade <tej.patade69@gmail.com>
2023-09-12 09:53:47 +0530

    Starting Off with Basics

--- Sample.cpp
+++ Sample.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-  int var;
+  string var;
   cout << "Enter Your Name: ";
   cin >> var;
+  cout << "Helllo " << var << endl;
 
   // 0 denotes no error to OS, any other value means the program encountered an error
   return 0;
